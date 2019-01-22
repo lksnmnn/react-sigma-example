36c6b83943e43c7d410e9cf9fbb874dd0b96ed9c
Lukas Neumann <lksnmnn@gmail.com>
2019-01-22 10:28:38 +0100

    Add sigma.js and basic example

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {UndirectedGraph} from 'graphology';
+import complete from 'graphology-generators/classic/complete';
+import randomLayout from 'graphology-layout/random';
+import chroma from 'chroma-js';
+import faker from 'faker';
+import WebGLRenderer from 'sigma/renderers/webgl';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <div id="container"></div>
       </div>
     );
+  }
+
+  componentDidMount() {
+    const container = document.getElementById("container");
+
+    const graph = complete(UndirectedGraph, {order: 10});
+    randomLayout.assign(graph);
+    
+    graph.nodes().forEach(node => {
+      const attr = graph.getNodeAttributes(node);
+    
+      graph.mergeNodeAttributes(node, {
+        label: faker.name.findName(),
+        size: Math.max(4, Math.random() * 10),
+        color: chroma.random().hex()
+      });
+    });
+    
+    const renderer = new WebGLRenderer(graph, container);
+    
+    window.graph = graph;
+    window.renderer = renderer;
+    window.camera = renderer.camera;
   }
 }
 
